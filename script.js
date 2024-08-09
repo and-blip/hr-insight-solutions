89be3ec8e94b4ab1b1536640ddc3744ba4da6bf8
and-blip <nieto.d.anthony@gmail.com>
2024-08-08 21:56:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (name && email && message) {
+        alert('Thank you for your message, ' + name + '. We will get back to you shortly.');
+        // Here, you would typically send the form data to a server
+        document.getElementById('contactForm').reset();
+    } else {
+        alert('Please fill in all fields.');
+    }
+});
